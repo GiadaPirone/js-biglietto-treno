e74f09dd46b340f8f59ea48d666fdd00fc2d80ac
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-04-13 18:08:01 +0200

    inserimento tofixed

--- js/main.js
+++ js/main.js
@@ -6,20 +6,30 @@ let età = prompt ( "inserisci la tua età");
 let prezzoBiglietto = prezzoPerKm * chilometri;
 
 
-
+//il passeggero ha < 18 anni
 if (età < 18){
     prezzoBigliettoSconto = prezzoBiglietto - (prezzoBiglietto * 20 /100);
     alert ("hai avuto uno sconto del 20%")
+} 
+// il passeggero ha + o = 18 anni
+if( età==18 ||età>18){
+    alert ("pagherai il biglietto a prezzo intero");
+    document.getElementById("prezzoFinale").innerText = prezzoBiglietto.toFixed(2);
 }
 
 
-
+//il passeggero ha >65 anni
 if ( età > 65){
     prezzoBigliettoSconto = prezzoBiglietto - (prezzoBiglietto * 40 / 100);
     alert ("hai avuto uno sconto del 40%")
+} 
+// il passeggero ha > o = 18 anni
+if( età ==65 || età < 65){
+    document.getElementById("prezzoFinale").innerText = prezzoBiglietto.toFixed(2);
 }
 
-document.getElementById("prezzoFinale").innerText = prezzoBigliettoSconto;
+document.getElementById("prezzoFinale").innerText = prezzoBigliettoSconto.toFixed(2);
+
 
   
 
